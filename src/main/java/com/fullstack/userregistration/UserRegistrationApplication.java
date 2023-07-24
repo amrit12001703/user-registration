25f271ba2cccc54c4e11f93d9acc4c0dc0a1e405
Amrit Anand <amritanand8271@gmail.com>
2023-07-24 16:02:46 +0530

    Add Custom Error Controller Dev env 2

--- src/main/java/com/fullstack/userregistration/UserRegistrationApplication.java
+++ src/main/java/com/fullstack/userregistration/UserRegistrationApplication.java
@@ -2,8 +2,10 @@ package com.fullstack.userregistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.fullstack.userregistration"})
 public class UserRegistrationApplication {
 
 	public static void main(String[] args) {
